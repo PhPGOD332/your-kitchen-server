1b1fbf36db024cf9c7abb56d685adb7d89986823
Timur <tima.gizatullin.01@mail.ru>
2025-05-23 20:35:35 +0500

    Исправил конфиг для pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,6 @@ module.exports = {
       script: 'dist/api/index.js',
       args: '-p 3001',
       exec_mode: 'cluster',
-      instances: 2,
       max_memory_restart: "800M",
       autorestart: true
     }
